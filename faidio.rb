323a22823cf6b46b21eb410b4e9f6e7c3bb27397
NieXS <neo.niexs@gmail.com>
2009-05-28 21:44:55 -0300

    THE MODULE REVOLUTION
    Modularized the long methods, wiki, macros and faidio

---
+++ faidio.rb
@@ -0,0 +1,46 @@
+module Faidio
+	def faidio(target)
+		# Threads!
+		Thread.new do
+		Net::HTTP.start("sajuuk.org",8001) do |http|
+			req = Net::HTTP::Get.new("/admin/stats.xml")
+			req.basic_auth("admin","854600")
+			reply = http.request(req)
+			$xmlfile = reply.body
+		end
+#		puts @xmlfile
+		$xmlfile = REXML::Document.new $xmlfile
+		@root = $xmlfile.root
+#		xmlfile.elements.each("icestats/sources") { |sources| $sources_num = sources.text.to_i }
+		if @root.elements["sources"].text.to_i >= 1 then
+			privmsg(target,"Streams:")
+			$xmlfile.elements.each("icestats/source") do |source|
+				mountpoint = source.attributes["mount"]
+				listeners_plus_peak = source.elements["listeners"].text+"/"+source.elements["listener_peak"].text
+				artist_title = source.elements["artist"].text+" - "+source.elements["title"].text
+				name_description = source.elements["server_name"].text+" ("+source.elements['server_description'].text+")"
+				if source.elements["listenurl"] != nil then
+					listen_url = source.elements["listenurl"].text
+				else
+					listen_url = ""
+				end
+				samplerate = source.elements["audio_samplerate"].text
+				if source.elements["ice-channels"] != nil then
+					channels = (source.elements["ice-channels"].text.to_i>1 ? "Stereo" : "Mono")
+				elsif source.elements["audio_channels"] != nil then
+					channels = (source.elements["audio_channels"].text.to_i>1 ? "Stereo" : "Mono")
+				else
+					channels = "unknown"
+				end
+				genre = source.elements["genre"].text
+				subtype = source.elements["subtype"].text
+				output = name_description+" at "+listen_url+"; Genre is "+genre+"; "+listeners_plus_peak+" listeners. Streaming in "+channels+" at "+samplerate+" Hz using "+subtype+". Now playing: "+artist_title
+				privmsg(target,output)
+			end
+				
+		else
+			privmsg(target,"No one is streaming.")
+		end
+	end
+	end
+end
